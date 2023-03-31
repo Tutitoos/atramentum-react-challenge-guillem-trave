50f6701424e3bc369e1e17a4d845474f35380554
tutitoos <tutitoosdev@gmail.com>
2023-03-31 12:24:59 +0200

    Add mock renderWithProviders and test Header

---
+++ src/__tests__/components/Header.test.tsx
@@ -0,0 +1,21 @@
+import Header from "@/components/Header/Header";
+import renderWithProviders from "@/__mocks__/renderWithProviders";
+import { screen } from "@testing-library/react";
+
+describe("Given a Header component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show on the screen a header title", () => {
+      const expectedTitle = "Atramentum";
+
+      renderWithProviders(<Header />);
+
+      const resultTitle = screen.queryByRole("heading", {
+        level: 1,
+        name: expectedTitle,
+      }) as HTMLHeadingElement;
+
+      expect(resultTitle).toBeInTheDocument();
+      expect(resultTitle.textContent).toStrictEqual(expectedTitle);
+    });
+  });
+});
