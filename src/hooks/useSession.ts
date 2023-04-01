96ad2b1005fc10a5b30c982857c7f9909c682300
tutitoos <tutitoosdev@gmail.com>
2023-04-02 01:02:30 +0200

    Fix and refactor useSession

--- src/hooks/useSession.ts
+++ src/hooks/useSession.ts
@@ -1,7 +1,6 @@
 import type { Session } from "@/types/clientTypes";
 import axios from "axios";
 
-const webUrl = process.env.NEXT_PUBLIC_WEB_URL;
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 const adminUsername = process.env.NEXT_PUBLIC_ADMIN_USERNAME;
 const adminPassword = process.env.NEXT_PUBLIC_ADMIN_PASSWORD;
@@ -17,7 +16,6 @@ const useSession = () => {
         },
         {
           headers: {
-            Accept: "application/json",
             "Content-Type": "application/x-www-form-urlencoded",
           },
         }
@@ -31,30 +29,13 @@ const useSession = () => {
   };
 
   const getToken = async (): Promise<string | null> => {
-    try {
-      const response = await axios.get<string>(`${webUrl}/api/session`);
-      const responseData = response.data;
+    const response = await getSession();
+    if (!response) return null;
 
-      return responseData;
-    } catch {
-      return null;
-    }
+    return response.token;
   };
 
-  const setToken = async (token: string): Promise<string | null> => {
-    try {
-      const response = await axios.post<string>(`${webUrl}/api/session`, {
-        sessionToken: token,
-      });
-      const responseData = response.data;
-
-      return responseData;
-    } catch {
-      return null;
-    }
-  };
-
-  return { getSession, getToken, setToken };
+  return { getSession, getToken };
 };
 
 export default useSession;
