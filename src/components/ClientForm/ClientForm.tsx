d3849c13ce9ba9d4dd9fb62e2f4a91b79c36970f
tutitoos <tutitoosdev@gmail.com>
2023-04-02 01:22:53 +0200

    Add component ClientForm and implement with page client/id

--- src/components/ClientForm/ClientForm.tsx
+++ src/components/ClientForm/ClientForm.tsx
@@ -1,81 +1,151 @@
+import useApi from "@/hooks/useApi";
+import { Client, ClientUpdate } from "@/types/clientTypes";
+import { useState } from "react";
 import Field from "../Field/Field";
 import ClientFormStyled from "./ClientFormStyled";
 
-const ClientForm = (): JSX.Element => {
+const ClientForm: React.FC<{
+  client: Client;
+}> = ({ client }): JSX.Element => {
+  const { updateClient } = useApi();
+  const [formData, setFormData] = useState<Client>(client);
+
+  const handleSubmit = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    const newClient: ClientUpdate = {
+      contactName: formData.contactName,
+      email: formData.email,
+      phone1: formData.phones[0],
+      phone2: formData.phones[1],
+      preferredCompanyBankAccountId: formData.bankAccount.id,
+      customerCategoryId: formData.category.id,
+      sectorId: formData.sector.id,
+      deleted: formData.deleted,
+      activated: formData.actived,
+      observations: formData.observations,
+    };
+
+    const res = await updateClient(client.id, newClient);
+    console.log(res);
+  };
+
   return (
-    <ClientFormStyled className="form">
+    <ClientFormStyled className="form" onSubmit={handleSubmit}>
       <div className="form-fields">
         <Field
           field={{
             type: "TEXT",
+            onChange: ({ target }) => setFormData({ ...formData, contactName: target.value }),
           }}
           label="Name"
           name="name"
           required={true}
+          value={formData.contactName}
         />
         <Field
           field={{
             type: "EMAIL",
+            onChange: ({ target }) => setFormData({ ...formData, email: target.value }),
           }}
           label="Email"
           name="email"
           required={true}
+          value={formData.email}
         />
         <Field
           field={{
             type: "TEL",
+            onChange: ({ target }) => {
+              formData.phones[0] = target.value;
+              setFormData({ ...formData });
+            },
           }}
           label="Phone (Primary)"
           name="phonePrimary"
           required={true}
+          value={formData.phones[0]}
         />
         <Field
           field={{
             type: "TEL",
+            onChange: ({ target }) => {
+              formData.phones[1] = target.value;
+              setFormData({ ...formData });
+            },
           }}
           label="Phone (Secondary)"
           name="phoneSecondary"
-          required={false}
+          value={formData.phones[1]}
         />
         <Field
           field={{
             type: "NUMBER",
+            onChange: ({ target }) =>
+              setFormData({
+                ...formData,
+                bankAccount: {
+                  ...formData.bankAccount,
+                  id: target.valueAsNumber,
+                },
+              }),
           }}
           label="Bank Account"
           name="bankAccount"
           required={true}
+          value={`${formData.bankAccount.id}`}
         />
         <Field
           field={{
             type: "NUMBER",
+            onChange: ({ target }) =>
+              setFormData({
+                ...formData,
+                sector: {
+                  ...formData.sector,
+                  id: target.valueAsNumber,
+                },
+              }),
           }}
           label="Sector"
           name="sector"
           required={true}
+          value={`${formData.sector.id}`}
         />
         <Field
           field={{
             type: "NUMBER",
+            onChange: ({ target }) =>
+              setFormData({
+                ...formData,
+                category: {
+                  ...formData.category,
+                  id: target.valueAsNumber,
+                },
+              }),
           }}
           label="Category"
           name="category"
           required={true}
+          value={`${formData.category.id}`}
         />
         <Field
           field={{
             type: "CHECKBOX",
+            checked: formData.actived,
+            onChange: ({ target }) => setFormData({ ...formData, actived: target.checked }),
           }}
           label="Actived"
           name="actived"
-          required={true}
         />
         <Field
           field={{
             type: "CHECKBOX",
+            checked: formData.deleted,
+            onChange: ({ target }) => setFormData({ ...formData, deleted: target.checked }),
           }}
           label="Deleted"
           name="deleted"
-          required={true}
         />
         <Field
           field={{
@@ -84,10 +154,12 @@ const ClientForm = (): JSX.Element => {
               cols: 300,
               rows: 300,
             },
+            onChange: ({ target }) => setFormData({ ...client, observations: target.value }),
           }}
           label="Observations"
           name="observations"
           required={true}
+          value={formData.observations}
         />
       </div>
       <div className="form-actions">
--- src/pages/client/[id].tsx
+++ src/pages/client/[id].tsx
@@ -1,17 +1,38 @@
 import ClientForm from "@/components/ClientForm/ClientForm";
+import useApi from "@/hooks/useApi";
+import { loadClientActionCreator } from "@/redux/features/clientSlice/clientSlice";
+import { showLoadingActionCreator, hideLoadingActionCreator } from "@/redux/features/uiSlice/uiSlice";
+import { wrapper } from "@/redux/store";
 import ClientFormPageStyled from "@/styles/pages/ClientFormPageStyled";
-import { useRouter } from "next/router";
+import { GetServerSideProps } from "next/types";
+import { useAppSelector } from "@/redux/hooks";
+import { parseClient } from "@/utils/parseClient";
 
 const ClientPage = (): JSX.Element => {
-  const router = useRouter();
-  const { id } = router.query;
+  const client = useAppSelector((state) => state.client.client);
 
   return (
     <ClientFormPageStyled>
-      <h2>Client {id}</h2>
-      <ClientForm />
+      <h2>Client {client.id}</h2>
+      <ClientForm client={client} />
     </ClientFormPageStyled>
   );
 };
 
 export default ClientPage;
+
+export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps((store) => async (ctx) => {
+  const { id } = ctx.query as {
+    id: string;
+  };
+
+  const { getClient } = useApi();
+  store.dispatch(showLoadingActionCreator());
+  const response = await getClient(id);
+  if (response) store.dispatch(loadClientActionCreator(parseClient(response)));
+  store.dispatch(hideLoadingActionCreator());
+
+  return {
+    props: {},
+  };
+});
