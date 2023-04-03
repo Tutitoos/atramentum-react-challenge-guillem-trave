ba7e2ef0d9e3eaf5d5545553242229ed4a3d2d12
tutitoos <tutitoosdev@gmail.com>
2023-04-03 17:21:16 +0200

    Remove feedback modals

--- src/redux/features/uiSlice/reducers/showModal.ts
+++
@@ -1,9 +0,0 @@
-import { PayloadAction } from "@reduxjs/toolkit";
-import { UiState, UiModalState } from "../../../../types/uiTypes";
-
-const showModal = (previousState: UiState, action: PayloadAction<UiModalState>) => ({
-  ...previousState,
-  modal: { ...action.payload, isOpen: true },
-});
-
-export default showModal;
